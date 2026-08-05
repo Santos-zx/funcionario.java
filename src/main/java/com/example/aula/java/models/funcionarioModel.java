d0505034d6581b4731acfb0104ad8f316395be65
Santos-zx <jose.augustolima.sant@gmail.com>
2026-08-04 21:54:11 -0300

    funcionario

---
+++ src/main/java/com/example/aula/java/models/funcionarioModel.java
@@ -0,0 +1,43 @@
+package com.example.aula.java.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity new
+public class funcionarioModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+  private String nome;
+  private String cpf;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public funcionarioModel(){
+
+}
+
