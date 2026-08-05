8b0b418f93991af27ca12fec4254e9c37ac89fa8
Santos-zx <jose.augustolima.sant@gmail.com>
2026-08-04 22:22:00 -0300

    service

---
+++ src/main/java/service/funcionario/sevice.java
@@ -0,0 +1,13 @@
+package service.funcionario;
+
+import com.example.aula.java.models.FuncionarioModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
+
+import java.awt.*;
+
+public class sevice {
+    @Autowired
+private
+    }
+
